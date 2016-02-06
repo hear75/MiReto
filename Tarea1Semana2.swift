8642dacaa0192356fa983867fea13c11286cd9e3
RAUL HERRERA ATRISCO <hear75@mbpderaaatrisco.lan>
2016-02-06 14:48:32 -0600

    commit

---
+++ Tarea1Semana2.swift
@@ -0,0 +1,9 @@
+//
+//  Tarea1Semana2.swift
+//  MIRetoRaulHerrera
+//
+//  Created by RAUL HERRERA ATRISCO on 06/02/16.
+//  Copyright © 2016 RAUL HERRERA ATRISCO. All rights reserved.
+//
+
+import Foundation
